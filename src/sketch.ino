776197bcd93e007a81ac937ec1b1a0f6c9118c38
Fukuo Kadota <app.fk91@gmail.com>
2015-02-03 03:03:54 +0900

    Success serial writing

--- src/sketch.ino
+++ src/sketch.ino
@@ -6,16 +6,17 @@ int val = 0;
 
 void setup()
 {
+  Serial.begin(9600);
   pinMode(LED, OUTPUT);
   pinMode(BUTTON, INPUT);
 }
 
 void loop()
 {
-  Serial.println("Serial test. looop");
   val = digitalRead(BUTTON);
 
   if(val == HIGH){
+    Serial.println("button pushing");
     digitalWrite(LED, HIGH);
   }else if(val == LOW){
     digitalWrite(LED, LOW);
