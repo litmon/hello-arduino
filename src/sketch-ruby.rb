94173cd2ecc686d5a9bd7b95e976ed76fb50a6f2
Fukuo Kadota <app.fk91@gmail.com>
2015-02-04 14:22:37 +0900

    Fix first led turned off

--- src/sketch-ruby.rb
+++ src/sketch-ruby.rb
@@ -4,6 +4,9 @@ board = Dino::Board.new(Dino::TxRx.new)
 led = Dino::Components::Led.new(pin: 13, board: board)
 button = Dino::Components::Button.new(pin: 7, board: board)
 
+sleep 0.1
+led.off
+
 button.up do
   led.send(:off)
 end
